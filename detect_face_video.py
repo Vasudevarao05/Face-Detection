6c7212df4c8e0fb6d850d2062adb4f64637892df
Vasudevarao05 <81547739+Vasudevarao05@users.noreply.github.com>
2021-03-29 15:29:28 +0530

    removed video file

--- detect_face_video.py
+++ detect_face_video.py
@@ -4,7 +4,7 @@ import cv2
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # To capture video from webcam. 
-cap = cv2.VideoCapture(vasu.mp4)
+cap = cv2.VideoCapture(0)
 # To use a video file as input
 # cap = cv2.VideoCapture('filename.mp4')
 

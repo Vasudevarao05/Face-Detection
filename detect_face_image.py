6e0e2b30ca65e5509f04cb14da9cbfba6684af86
Vasudevarao05 <81547739+Vasudevarao05@users.noreply.github.com>
2021-03-29 14:44:07 +0530

    random comment

--- detect_face_image.py
+++ detect_face_image.py
@@ -13,6 +13,7 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
 # Draw rectangle around the faces
+#comment demo
 for (x, y, w, h) in faces:
     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
